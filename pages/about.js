31da9c2cff44d429cb60f27ddd908fa488b6652a
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2022-02-05 11:28:19 -0600

    fix(deployment): 🚑️ fix a compile issue

--- pages/about.js
+++ pages/about.js
@@ -9,47 +9,46 @@ export default function About() {
       <Meta />
 
       <main className={styles.main}>
-        <h1 className={styles.title}>About This Site</h1>
-        <span>&#8595;</span>
-        <p className={styles.description}>
-          It&apos;s built with React + Next.js
-        </p>
-        <span>&#8595;</span>
-        <p className={styles.description}>
-          Using <code>create-next-app</code>
-        </p>
-        <span>&#8595;</span>
-        <p className={styles.description}>
-          Here&apos;s the{" "}
-          <Link href="https://github.com/anthonyjdella/roadmap">
-            <a className={styles.link2} target="_blank">
-              GitHub repo
-            </a>
-          </Link>
-        </p>
-
-        <p className={styles.description}>
-          <Link href="https://anthonydellavecchia.com">
-            <a className={styles.link} target="_blank">
-              Anthony
-            </a>
-          </Link>
-          &nbsp;created this site (from Feb 3-6, 2022) to showcase his skills &
-          passion for{" "}
-          <Link href="https://devrel.co/about/">
-            <a className={styles.link2} target="_blank">
-              DevRel
-            </a>
-          </Link>
-        </p>
+          <h1 className={styles.title}>About This Site</h1>
+          <span>&#8595;</span>
+          <p className={styles.description}>
+            <Link href="https://anthonydellavecchia.com">
+              <a className={styles.link} target="_blank">
+                Anthony
+              </a>
+            </Link>
+            &nbsp;created this site (from Feb 3-6, 2022) to showcase his skills
+            & passion for{" "}
+            <Link href="https://devrel.co/about/">
+              <a className={styles.link2} target="_blank">
+                DevRel
+              </a>
+            </Link>
+          </p>
+          <p className={styles.description}>
+            It&apos;s built with React + Next.js
+          </p>
+          <span>&#8595;</span>
+          <p className={styles.description}>
+            Using <code>create-next-app</code>
+          </p>
+          <span>&#8595;</span>
+          <p className={styles.description}>
+            Here&apos;s the{" "}
+            <Link href="https://github.com/anthonyjdella/roadmap">
+              <a className={styles.link2} target="_blank">
+                GitHub repo
+              </a>
+            </Link>
+          </p>
 
         <div className={styles.space}></div>
 
         <h1 className={styles.title}>About Anthony</h1>
 
         <blockquote>
-          "I can empathize and communicate with developers because I am a
-          developer."
+          &quot;I can empathize and communicate with developers because I am a
+          developer.&quot;
           <span>Anthony Dellavecchia</span>
         </blockquote>
 
