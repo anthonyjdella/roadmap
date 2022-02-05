cfb6d8a520ec5abbe44a72cbb847d8cd9b024709
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2022-02-05 12:42:18 -0600

    feat(top5): ✨ Adding my Top 5 blog posts

--- pages/my-top-5.js
+++ pages/my-top-5.js
@@ -10,17 +10,57 @@ export default function MyTop5() {
 
         <main className={styles.main}>
           <h1 className={styles.title}>My Top 5</h1>
-          <h2>These are some of my favorite technical blog posts.</h2>
+          <h2>
+            These are some of my favorite technical blog posts (which I&apos;ve
+            written).
+          </h2>
 
           <article>
             <ol className={stylesTop5.list}>
-              <li>My First Blog Post, in 2018</li>
-              <iframe src="https://dev.to/anthonyjdella/why-you-should-use-a-developer-font-2gio" />
-              <iframe src="https://medium.com/@anthonyjdella/how-to-never-miss-out-on-a-job-opening-node-js-with-puppeteer-d46f23139802" />
-              <li>Solving a Scaling Issue with Personal Blogs</li>
-              <li>My Most Talked About & Engaging Post</li>
-              <li>My First Featured Post</li>
-              <li>Helping Developers Solve a Compatibility Issue</li>
+              <li className={stylesTop5.listItem}>
+                My First Blog Post, in 2018
+              </li>
+              <iframe
+                className={`${stylesTop5.iframe} ${stylesTop5.iframe1}`}
+                src="https://anthonydellavecchia.com/blog/how-to-never-miss-out-on-a-job-opening/"
+                width="1028"
+                height="600"
+              />
+              <li className={stylesTop5.listItem}>
+                Solving a Scaling Issue with Personal Blogs
+              </li>
+              <iframe
+                className={`${stylesTop5.iframe} ${stylesTop5.iframe2}`}
+                src="https://anthonydellavecchia.com/blog/how-to-add-search-to-your-hugo-static-site/"
+                width="1028"
+                height="600"
+              />
+              <li className={stylesTop5.listItem}>
+                {" "}
+                My Most Talked About & Engaging Post
+              </li>
+              <iframe
+                className={`${stylesTop5.iframe} ${stylesTop5.iframe3}`}
+                src="https://anthonydellavecchia.com/blog/rogue-dev-purposely-breaks-software/"
+                width="1028"
+                height="600"
+              />
+              <li className={stylesTop5.listItem}>My First Featured Post</li>
+              <iframe
+                className={`${stylesTop5.iframe} ${stylesTop5.iframe4}`}
+                src="https://anthonydellavecchia.com/blog/fonts-for-programmers/"
+                width="1028"
+                height="600"
+              />
+              <li className={stylesTop5.listItem}>
+                Helping Developers Solve a Compatibility Issue
+              </li>
+              <iframe
+                className={`${stylesTop5.iframe} ${stylesTop5.iframe5}`}
+                src="https://anthonydellavecchia.com/blog/mongodb-atlas-hackathon/"
+                width="1028"
+                height="600"
+              />
             </ol>
           </article>
 
